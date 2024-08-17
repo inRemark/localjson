4f44d018be30c450c0426cf26a0349e461cb3398
remark <about.remark@outlook.com>
2024-08-17 23:53:31 +0800

    update vite config

--- frontend/src/stores/fileUtils.ts
+++ frontend/src/stores/fileUtils.ts
@@ -1,6 +1,7 @@
 import {defineStore} from 'pinia';
 import {isBrowser, isWebView} from "@/utils/runtime.type";
 import {useDownloadFileFromBase64} from "@/composable/downloadBase64";
+import { SaveBase64File } from '../../wailsjs/go/services/fileService';
 
 export const useFileStore = defineStore('file', {
     state: () => ({
@@ -10,9 +11,9 @@ export const useFileStore = defineStore('file', {
     actions: {
         async saveFile(filename: string, data: any) {
             if (isWebView()) {
-                import('wailsjs/go/services/fileService.js').then(({SaveBase64File}) => {
+                // import('wailsjs/go/services/fileService.js').then(({SaveBase64File}) => {
                     SaveBase64File(data.value, filename);
-                });
+                // });
             } else {
                 console.log("is not WebView");
                 const {download} = useDownloadFileFromBase64({
