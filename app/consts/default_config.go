8d223ccd171ae8c8f716164a6e5e2627f17c60c2
remark <about.remark@outlook.com>
2024-09-22 19:48:49 +0800

    update package

--- app/consts/default_config.go
+++ app/consts/default_config.go
@@ -2,8 +2,8 @@ package consts
 
 const DefaultFontSize = 14
 const DefaultAsideWidth = 300
-const DefaultWindowWidth = 1024
-const DefaultWindowHeight = 768
+const DefaultWindowWidth = 1280
+const DefaultWindowHeight = 800
 const MinWindowWidth = 960
 const MinWindowHeight = 640
 const DefaultScanSize = 3000
