b1f9e424587a50fd7016951de1a586071056bf2b
inRemark <inremark@outlook.com>
2025-11-12 16:29:39 +0800

    clean desktop code

--- packages/desktop/scripts/getLatestChangelog.mjs
+++
@@ -1,6 +0,0 @@
-import { readFile } from 'node:fs/promises';
-
-const changelogContent = await readFile('./CHANGELOG.md', 'utf-8');
-const [, lastChangelog] = changelogContent.split(/^## .*$/gm);
-
-console.log(lastChangelog.trim());
